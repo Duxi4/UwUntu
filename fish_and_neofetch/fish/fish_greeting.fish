c76d0139ff40bbb869771869c21001af55f01012
Duxi <71901436+Duxi4@users.noreply.github.com>
2022-10-21 12:27:43 +0200

    .

--- fish_and_neofetch/fish/fish_greeting.fish
+++
@@ -1,7 +0,0 @@
-function fish_greeting
-	if string match -q "xterm-kitty" $TERM
-  		neofetch  --backend kitty --source /home/$USER/.config/neofetch/neofetch-logo.png
-	else
-  		neofetch
-	end
-end
